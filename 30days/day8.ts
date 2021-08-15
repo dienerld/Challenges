9af9750ba7159174fb0a6b47248466f613b7245f
Diener Dornelas <dienezim@aluno.ufsj.edu.br>
2021-08-15 18:58:42 -0300

    update function

--- 30days/day8.ts
+++ 30days/day8.ts
@@ -1,36 +1,15 @@
-interface Dictionary {
-	name: String;
-	phone: String;
-}
-
-function phoneList(contact: String): Dictionary {
-	const vecContact = contact.split(' ');
-	const contactDictionary: Dictionary = {
-		name: vecContact[0],
-		phone: vecContact[1],
-	};
-	return contactDictionary;
-}
-
 function main() {
 	const n: number = parseInt(readLine());
-	let isFind: boolean = false;
-	let arr: Dictionary[] = [];
+	let phoneList: any = {};
 	for (let i = 0; i < n; i++) {
-		const string: String = readLine();
-		arr.push(phoneList(string));
+		const stringNumber: string[] = readLine().split(' ');
+		phoneList[stringNumber[0]] = stringNumber[1];
 	}
 	for (let i = 0; i < n; i++) {
-		const string: String = readLine();
-		isFind = false;
-		arr.forEach(element => {
-			if (element.name == string) {
-				console.log(`${element.name}=${element.phone}`);
-				isFind = true;
-				return;
-			}
-		});
-		if (!isFind) {
+		const stringName: string = readLine();
+		if (stringName in phoneList) {
+			console.log(`${stringName}=${phoneList[stringName]}`);
+		} else {
 			console.log('Not found');
 		}
 	}
