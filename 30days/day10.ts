be5ca3ad8760c192b605228cddd2fadb14691628
Diener Dornelas <dienezim@aluno.ufsj.edu.br>
2021-08-15 19:00:34 -0300

    days 9-11 resolutions

---
+++ 30days/day10.ts
@@ -0,0 +1,17 @@
+const convertDecimalToBinary = (number: number) => {
+	const binary: string = (number >>> 0).toString(2);
+	let count: number = 0,
+		max: number = 0;
+	const length = binary.length;
+	for (let i = 0; i < length; i++) {
+		if (binary[i] == '1') {
+			count++;
+		} else {
+			count = 0;
+		}
+		max = max > count ? max : count;
+	}
+	console.log(max);
+};
+
+convertDecimalToBinary(125);
