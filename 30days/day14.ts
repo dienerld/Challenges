bff286a5d00648547e404229860287878e7fd043
Diener Dornelas <dienezim@aluno.ufsj.edu.br>
2021-08-31 13:30:00 -0300

    Solution 30days/day14

---
+++ 30days/day14.ts
@@ -0,0 +1,17 @@
+function main() {
+	const n = parseInt(readLine());
+	const elements = readLine().split(' ');
+	let absolute = -100000000;
+
+	for (let index = 0; index < n; index++) {
+		for (let index2 = 0; index2 < n; index2++) {
+			const tempAbsolute = Math.abs(
+				parseInt(elements[index], 10) - parseInt(elements[index2], 10)
+			);
+			if (absolute < tempAbsolute) {
+				absolute = tempAbsolute;
+			}
+		}
+	}
+	console.log(absolute);
+}
